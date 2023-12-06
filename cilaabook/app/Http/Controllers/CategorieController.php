7a0f30079040bbddece8d61c5f2821c12628a214
Boyor Annie Thérèse BADIANE <boyorthrse@gmail.com>
2023-12-06 19:59:51 +0000

    fonctionnalité/modifierCategorie

--- cilaabook/app/Http/Controllers/CategorieController.php
+++ cilaabook/app/Http/Controllers/CategorieController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\Categorie;
 use App\Http\Requests\StoreCategorieRequest;
 use App\Http\Requests\UpdateCategorieRequest;
+use App\Http\Requests\ModifierCategorieRequest;
 
 class CategorieController extends Controller
 {
@@ -51,9 +53,22 @@ class CategorieController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCategorieRequest $request, Categorie $categorie)
+    public function update(ModifierCategorieRequest $request, $id)
     {
-        //
+        try{
+           
+        $categorie=Categorie::findOrFail($id);
+        $categorie->nom=$request->nom;
+        $categorie->save();
+
+        return response()->json([
+        'status_code' =>200,
+        'status_message' =>'la categorie est ajoutée avec succès',
+        'data' =>$categorie
+        ]);
+    } catch (Exception $e) {
+       return response()->json($e);
+    }
     }
 
     /**
