c6e9d6fe143ee1d6c363ccc69fbf4eeca49a9a12
Mariama Diallo <maridialloisidk@gmail.com>
2023-12-07 17:23:52 +0000

    Validations des données des bailleurs et porteurs de projet

--- cilaabook/app/Http/Requests/StorePorteurprojetRequest.php
+++ cilaabook/app/Http/Requests/StorePorteurprojetRequest.php
@@ -24,12 +24,12 @@ class StorePorteurprojetRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nom' => 'required',
+            'nom' => 'required|min:5|regex:/^[a-zA-Z\s]+$/',
             'email' => 'required|email|unique:porteurprojets,email',
-            'password' => 'required',
+            'password' => 'required|min:8',
+            'adresse' => 'required|min:5|regex:/^[a-zA-Z\s]+$/',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif',
-            'adresse' => 'required',
-            'telephone' => 'required',
+            'telephone' => 'required|regex:/^(70|75|76|77|78)[0-9]{7}$/',
         ];
     }
     public function failedValidation(validator $validator ){
@@ -47,8 +47,8 @@ class StorePorteurprojetRequest extends FormRequest
             'nom.required'=> 'Le champs nom est obligatoire',
             'email.required'=> 'Le champs email est obligatoire',
             'adresse.required'=> 'Le champs adresse est obligatoire',
-            'telephone.required'=> 'Le champs telephone est obligatoire',
-            'password.required'=> 'Le champs mot de passe est obligatoire',
+            'telephone.required'=> 'Le champs telephone est obligatoire et doit commencer par 78 ou 77 ou 76 ou 70',
+            'password.required'=> 'Le champs mot de passe est obligatoire et doit contenir minimum 8 caracteres',
         ];
     }
 }
